a9f680cf28394c706ccfbaa872ba817a5f292f92
95sid <nobbie7579@gmail.com>
2024-01-16 04:57:57 +0530

    c

--- JAVASCRIPT/01_BASICS/tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-ateStyle:"full"
--- JAVASCRIPT/03_BASICS/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-this ==> this keyword refers the current context (values)
-const user = {
-    name:"Siddhant",
-    price: 999,
-    welcomeMessage:function(){
-        console.log(`${this.name}, welcome to our Website`);
-        // console.log(this) // ==>print current context 
-    }
-}
