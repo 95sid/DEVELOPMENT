f6c35453efb2925b8e59e582a09af896c22f9b02
95sid <nobbie7579@gmail.com>
2024-02-05 22:29:47 +0530

    finallybuildweatherapp

--- JAVASCRIPT/11_Projects/05_weatherapp/index.js
+++ JAVASCRIPT/11_Projects/05_weatherapp/index.js
@@ -1,19 +1,44 @@
-const data = "46182dbe893ba516dad98e7a4d85cf41";
+const apikey = "46182dbe893ba516dad98e7a4d85cf41";
 const screen = document.querySelector(".screen");
 const submit = document.querySelector("button");
+submit.addEventListener("click", function (e) {});
 
-submit.addEventListener("click",function(e){
-    
-})
-
-async function getWeather(value) {
+submit.addEventListener("click", (event) => {
+  const cityName = screen.value;
+  getWeather(cityName);
+});
+async function getWeather(cityvalue) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q = ${cityvalue}&appid=${appkey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityvalue}&appid=${apikey}&u nits=metric`
     );
-    if (!response.ok) throw new Error("Network Error");
+    if (!response.ok) throw new Error("Network Error: Enter Valid City Name");
     const data = await response.json();
     console.log(data);
-  } catch (error) {}
-}
 
+    document.querySelector(".feels-like").innerHTML = `Feel Like: ${(
+      data.main.feels_like - 273
+    ).toFixed(2)} `;
+
+    document.querySelector(
+      ".Humidity"
+    ).innerHTML = `Humidity:${data.main.humidity}`;
+
+    document.querySelector(
+      ".wind-speed"
+    ).innerHTML = `Wind Speed: ${data.wind.speed}`;
+
+    document.querySelector(".temp").innerHTML = (data.main.temp - 273).toFixed(
+      1
+    );
+
+    document.querySelector(".conditions").innerHTML =
+      data.weather[0].description;
+    document.querySelector(".weather-icon").innerHTML = `<img
+    src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"
+    alt="Weather Icon"
+  />`;
+  } catch (error) {
+    alert(error);
+  }
+}
