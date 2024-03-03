471d40da74693fcc3e673d1d15c47b8a939ae568
95sid <nobbie7579@gmail.com>
2024-03-03 16:57:32 +0530

    update in calcluator

--- JAVASCRIPT/11_Projects/01_Calculator/index.js
+++ JAVASCRIPT/11_Projects/01_Calculator/index.js
@@ -3,12 +3,16 @@ let str = "";
 buttons.forEach((button) => {
   button.addEventListener("click", function (e) {
     if (e.target.innerHTML == "M+") {
+      // M+
+      str = "";
       document.querySelector("input").value = str;
     }
     if (e.target.innerHTML == "C") {
+      // C
       str = "";
       document.querySelector("input").value = str;
     } else if (e.target.innerHTML == "=") {
+      // =
       try {
         str = eval(str);
         document.querySelector("input").value = str;
@@ -16,8 +20,32 @@ buttons.forEach((button) => {
         document.querySelector("input").value = "Error";
       }
     } else {
-      str = str + e.target.innerHTML;
-      console.log(str);
+      if (
+        str.length !== 0 &&
+        (e.target.innerHTML == "-" ||
+          e.target.innerHTML == "+" ||
+          e.target.innerHTML == "/" ||
+          e.target.innerHTML == "*" ||
+          e.target.innerHTML == "%" ||
+          e.target.innerHTML == ".")
+      ) {
+        console.log("Entered: ");
+        let tempstr = str[str.length - 1];
+        if (
+          tempstr == "-" ||
+          tempstr == "/" ||
+          tempstr == "+" ||
+          tempstr == "*" ||
+          tempstr == "%" ||
+          tempstr == "."
+        ) {
+          str = str.slice(0, -1) + e.target.innerHTML;
+          console.log("Entered: ");
+        } else str = str + e.target.innerHTML;
+      } else {
+        str = str + e.target.innerHTML;
+        console.log(str);
+      }
       document.querySelector("input").value = str;
     }
   });
