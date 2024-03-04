7bede4f4957ff9792cbf7b949d5bdac5c2c28982
95sid <nobbie7579@gmail.com>
2024-03-04 21:39:21 +0530

    firstdayreact

---
+++ REACT/01_viteReact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./Chai"
+function App() {
+
+  return (
+    // Generally you can return 1 tag only
+    // to return multiple tags we can use fragments <></>
+
+    <>
+
+    <Chai/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ REACT/01_viteReact/src/Chai.jsx
@@ -0,0 +1,8 @@
+
+function Chai(){
+    return(
+        <h3>chai is Ready</h3>
+    )
+}
+
+export default Chai;
