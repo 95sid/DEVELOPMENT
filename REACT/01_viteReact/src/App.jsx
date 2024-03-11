95a7a0536b4b5f8ff60167bd9263d3285cfe08e7
95sid <nobbie7579@gmail.com>
2024-03-11 19:03:52 +0530

    day02

--- REACT/01_viteReact/src/App.jsx
+++ REACT/01_viteReact/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from "./Chai"
 function App() {
+  const UserName = "Siddhant"
 
   return (
     // Generally you can return 1 tag only
     // to return multiple tags we can use fragments <></>
-
     <>
-
+    <h1>Hello {UserName}</h1>
     <Chai/>
     </>
     
--- REACT/01_viteReact/src/main.jsx
+++ REACT/01_viteReact/src/main.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// JSX format we used 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+// JS format which is parsed from JSX by bundler
+// const ReactElement = {
+//   type:'a',
+//   props: {
+//     href:"https://google.com",
+//     target:'_blank'
+//   },
+//   children: "Click me to Visit Google"
+// }
+
+const anotherUser  = "Chai Aur Code"
+const newElement = React.createElement(
+  'a',
+  {href:"https://google.com",target:'_blank'},
+  "Click me To visit Google",
+  anotherUser
+)
+const anotherElement = (
+  <a href='Google.com'>Visit Google</a>
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <MyApp /> // this is a function 
+
+    // ReactElement // it is not working because it is custom object require some 
+    // operation to render 
+
+    // anotherElement // works
+
+    // newElement // it will work because we generate it by using createElement 
+    <App/>
+
 )
